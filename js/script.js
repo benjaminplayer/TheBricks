3fd1a5a2f7eff065d257611246918859a48d4c12
benjaminplayer <153460006+benjaminplayer@users.noreply.github.com>
2025-03-24 10:48:30 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const gameWindow = document.querySelector(".game");
 const ball = document.querySelector(".ball");
 const ctx = gameWindow.getContext('2d');
 
+let x = 75, y= 75, dx = 10, dy = 5;
+let recx = 100, recy = 20, recdx = 10;
 function keyPressedDonwn(e){
     
     if(e.keyCode === 65 && padLeft-5 >= 0){
@@ -27,19 +29,39 @@ function keyReleased(e){
 }
 
 drawPad();
-drawCircle();
+//drawCircle();
+
+requestAnimationFrame(moveBall);
+
 function drawPad(){
     ctx.beginPath();
-    ctx.rect(0, 800-20, 100, 20);
     ctx.closePath();
     ctx.fill();
 }
 
-function drawCircle(){
+/*function drawCircle(){
     ctx.beginPath();
     ctx.arc(75, 75, 10, 0, Math.PI*2, true);
     ctx.closePath();
     ctx.fill();
+}*/
+
+function moveBall(){
+    ctx.reset();
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2, true);
+    ctx.rect(0, 800-20, recx, 20);
+    ctx.closePath();
+    ctx.fill();
+
+    if(x+5 > gameWindow.width || x-5 < 0) 
+        dx *= -1;
+    if(y-5 < 0 || y+5 > gameWindow.height)
+        dy *=-1;
+    x += dx;
+    y += dy;
+
+    requestAnimationFrame(moveBall);
 }
 
 /*
